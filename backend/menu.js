ac8854baa898236fbb381edc55024672590bee0f
murdanetaos <119532654+murdanetaos@users.noreply.github.com>
2023-05-10 11:32:21 -0400

    update

---
+++ backend/menu.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+    fetch("menu.json")
+      .then((response) => response.json())
+      .then((data) => {
+        let productsHtml = "";
+        for (let i = 0; i < data.length; i++) {
+
+          let item = data[i];
+
+          let product = `
+            <div class="div${i+1}">
+              <img src="${item.img}" alt="carimg3">
+              <h2>${item.name}</h2>
+              <p class="price">${item.price}</p>
+              <span class="description">${item.description}</span>
+              <button type="text" class="submit">Add To Cart</button>
+            </div>
+          `;
+
+          productsHtml += product;
+        }
+
+
+        let productsContainer = document.getElementsByClassName("parent")[0]
+
+        productsContainer.innerHTML = productsHtml;
+      })
+      .catch((err) => console.log(err));
+  };
+  
